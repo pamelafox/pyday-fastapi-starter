cbbcbfe8c5102211b6ae277653b20b3fa36ac6c8
Pamela Fox <pamela.fox@gmail.com>
2023-04-27 21:33:03 +0000

    Pare down code

--- api/__init__.py
+++
@@ -1,9 +0,0 @@
-import nest_asyncio
-import azure.functions as func
-
-from .main import app
-
-nest_asyncio.apply()
-
-async def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
-    return func.AsgiMiddleware(app).handle(req, context)
--- api/main.py
+++ api/main.py
@@ -5,11 +5,7 @@ import fastapi
 app = fastapi.FastAPI()
 
 @app.get("/generate_name")
-async def generate_name(starts_with: str = None):
+async def generate_name():
     names = ["Minnie", "Margaret", "Myrtle", "Noa", "Nadia"]
-    if starts_with:
-        names = [n for n in names if n.lower().startswith(starts_with)]
-    if len(names) == 0:
-        raise fastapi.HTTPException(status_code=404, detail="No name found")
     random_name = random.choice(names)
     return {"name": random_name}
